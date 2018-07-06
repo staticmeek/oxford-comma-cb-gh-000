74614b9b64d64e8ec11d700390bad3311860c6b5
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-07-06 06:10:41 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.size == 2
     array.join(" and ")
   elsif array.size == 3
-    array.join(",")
-    array << array.join("and")
+    array.join(",") << ("and")
+    
   end
 end

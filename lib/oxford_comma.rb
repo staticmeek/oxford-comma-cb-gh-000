1bf63dd0ebc75e21fc3531efecd408747416ee4e
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-07-06 06:16:32 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,9 @@
 def oxford_comma(array)
-  if array.size == 1
-    array.join
-  elsif array.size == 2
+  if array.size > 2
+    last_val = array.pop.prepend("and ")
+    array.push(last_val)
+    array.join(", ")
+  else
     array.join(" and ")
-  elsif array.size == 3
-    array << (" and ")
-    array.join(",")
   end
 end

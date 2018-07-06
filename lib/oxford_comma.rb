a4a794def7c075011be26592888332e8186c95ae
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-07-06 06:07:18 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
-  if array.size = 1
+  if array.size == 1
     array.join
-  elsif array.size = 2
+  elsif array.size == 2
     array.join(" and ")
-  elsif array.size = 3
+  elsif array.size == 3
     array.split(" , ")
   end
 end
